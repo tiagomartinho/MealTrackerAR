c47dac6f76be37e537094ec5431c61105c524d3c
Tiago <tiago@macbook-pro-de-tiago.local>
2018-07-08 14:47:37 +0200

    Detect raw values of jaw and mouth

--- ARKitFaceExample/VirtualFaceContent/RobotHead.swift
+++ ARKitFaceExample/VirtualFaceContent/RobotHead.swift
@@ -11,6 +11,9 @@ import ARKit
 
 class RobotHead: SCNReferenceNode, VirtualFaceContent {
 
+    let jawOpenBuffer = RunningBuffer(size: 50)
+    let mouthClosedBuffer = RunningBuffer(size: 50)
+
     private var originalJawY: Float = 0
 
     private lazy var jawNode = childNode(withName: "jaw", recursively: true)!
@@ -39,16 +42,88 @@ class RobotHead: SCNReferenceNode, VirtualFaceContent {
         didSet {
             guard let eyeBlinkLeft = blendShapes[.eyeBlinkLeft] as? Float,
                 let eyeBlinkRight = blendShapes[.eyeBlinkRight] as? Float,
-                let jawOpen = blendShapes[.jawOpen] as? Float
+                let jawOpen = blendShapes[.jawOpen] as? Float,
+                let mouthClose = blendShapes[.mouthClose] as? Float
                 else { return }
             eyeLeftNode.scale.z = 1 - eyeBlinkLeft
             eyeRightNode.scale.z = 1 - eyeBlinkRight
-            jawNode.position.y = originalJawY - jawHeight * jawOpen
+            jawNode.position.y = originalJawY - jawHeight * (jawOpen + (1 - mouthClose))
         }
     }
     
     /// - Tag: ARFaceGeometryBlendShapes
     func update(withFaceAnchor faceAnchor: ARFaceAnchor) {
         blendShapes = faceAnchor.blendShapes
+        guard let jawOpen = faceAnchor.blendShapes[.jawOpen] as? Float,
+        let mouthClose = faceAnchor.blendShapes[.mouthClose] as? Float
+        else { return }
+        jawOpenBuffer.addSample(Double(jawOpen))
+        mouthClosedBuffer.addSample(Double(mouthClose))
+        if jawOpenBuffer.isFull() && mouthClosedBuffer.isFull() {
+            let jaw = jawOpenBuffer.recentMean()
+            let mouth = mouthClosedBuffer.recentMean()
+            print("\(jaw),\(mouth)")
+        }
     }
 }
+
+import Foundation
+
+class RunningBuffer {
+    
+    var size = 0
+    var buffer = [Double]()
+    
+    init(size: Int) {
+        self.size = size
+        buffer = [Double](repeating: 0.0, count: self.size)
+    }
+    
+    func addSample(_ sample: Double) {
+        buffer.insert(sample, at: 0)
+        if buffer.count > size {
+            buffer.removeLast()
+        }
+    }
+    
+    func reset() {
+        buffer.removeAll(keepingCapacity: true)
+    }
+    
+    func isFull() -> Bool {
+        return size == buffer.count
+    }
+    
+    func sum() -> Double {
+        return buffer.reduce(0.0, +)
+    }
+    
+    func min() -> Double {
+        var min = 0.0
+        if let bufMin = buffer.min() {
+            min = bufMin
+        }
+        return min
+    }
+    
+    func max() -> Double {
+        var max = 0.0
+        if let bufMax = buffer.max() {
+            max = bufMax
+        }
+        return max
+    }
+    
+    func recentMean() -> Double {
+        let recentCount = size / 2
+        var mean = 0.0
+        
+        if buffer.count >= recentCount {
+            let recentBuffer = buffer[0 ..< recentCount]
+            mean = recentBuffer.reduce(0.0, +) / Double(recentBuffer.count)
+        }
+        
+        return mean
+    }
+}
+
