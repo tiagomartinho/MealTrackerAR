9c05b7d88472cf783161fc13a132923b45154b6c
Tiago <tiago@macbook-pro-de-tiago.local>
2018-07-08 15:14:11 +0200

    Remove code

--- ARKitFaceExample/ViewController.swift
+++ ARKitFaceExample/ViewController.swift
@@ -4,22 +4,18 @@ import UIKit
 
 class ViewController: UIViewController, ARSessionDelegate {
     
-    @IBOutlet var sceneView: ARSCNView!
-    @IBOutlet weak var blurView: UIVisualEffectView!
-
     let jawOpenBuffer = RunningBuffer(size: 50)
     let mouthClosedBuffer = RunningBuffer(size: 50)
     
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var blurView: UIVisualEffectView!
+
     lazy var statusViewController: StatusViewController = {
         return childViewControllers.lazy.compactMap({ $0 as? StatusViewController }).first!
     }()
 
-    var session: ARSession {
-        return sceneView.session
-    }
+    var session: ARSession { return sceneView.session }
     
-    // MARK: - View Controller Life Cycle
-
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.delegate = self
@@ -43,7 +39,6 @@ class ViewController: UIViewController, ARSessionDelegate {
 
     func session(_ session: ARSession, didFailWithError error: Error) {
         guard error is ARError else { return }
-        
         let errorWithInfo = error as NSError
         let messages = [
             errorWithInfo.localizedDescription,
@@ -51,7 +46,6 @@ class ViewController: UIViewController, ARSessionDelegate {
             errorWithInfo.localizedRecoverySuggestion
         ]
         let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
-        
         DispatchQueue.main.async {
             self.displayErrorMessage(title: "The AR session failed.", message: errorMessage)
         }
@@ -67,42 +61,29 @@ class ViewController: UIViewController, ARSessionDelegate {
 
     func sessionInterruptionEnded(_ session: ARSession) {
         blurView.isHidden = true
-        
         DispatchQueue.main.async {
             self.resetTracking()
         }
     }
     
-    /// - Tag: ARFaceTrackingSetup
     func resetTracking() {
         statusViewController.showMessage("STARTING A NEW SESSION")
-        
         guard ARFaceTrackingConfiguration.isSupported else { return }
         let configuration = ARFaceTrackingConfiguration()
         configuration.isLightEstimationEnabled = true
         session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
 
-    // MARK: - Interface Actions
-
-    /// - Tag: restartExperience
     func restartExperience() {
-        // Disable Restart button for a while in order to give the session enough time to restart.
         statusViewController.isRestartExperienceButtonEnabled = false
         DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
             self.statusViewController.isRestartExperienceButtonEnabled = true
         }
-
         resetTracking()
     }
     
-    // MARK: - Error handling
-    
     func displayErrorMessage(title: String, message: String) {
-        // Blur the background.
         blurView.isHidden = false
-        
-        // Present an alert informing about the error that has occurred.
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
             alertController.dismiss(animated: true, completion: nil)
