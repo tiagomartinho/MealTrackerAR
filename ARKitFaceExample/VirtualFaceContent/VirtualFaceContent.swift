d519767e2e2b89b4e17e566ea7c8abce0d038907
Tiago <tiago@macbook-pro-de-tiago.local>
2018-07-08 15:04:39 +0200

    Remove code

--- ARKitFaceExample/VirtualFaceContent/VirtualFaceContent.swift
+++ ARKitFaceExample/VirtualFaceContent/VirtualFaceContent.swift
@@ -7,10 +7,3 @@ protocol VirtualFaceContent {
 
 typealias VirtualFaceNode = VirtualFaceContent & SCNNode
 
-func loadedContentForAsset(named resourceName: String) -> SCNNode {
-    let url = Bundle.main.url(forResource: resourceName, withExtension: "scn", subdirectory: "Models.scnassets")!
-    let node = SCNReferenceNode(url: url)!
-    node.load()
-    
-    return node
-}
