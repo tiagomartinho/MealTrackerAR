0400fc99304c867300a1c876e78394ba8f23e964
Tiago <tiago@MacBook-Pro-de-Tiago.local>
2018-07-18 11:56:58 +0200

    Group bite and chew related vars

--- Visualizer/visualizer.py
+++ Visualizer/visualizer.py
@@ -13,36 +13,49 @@ for file in files:
         dict[key] = dict.get(key, [0]) + value
     dataFile.close()
 
-# Related > 0.1
+# Related Common
 #del dict["jawOpen"]
+
+# Related Bite > 0.1
+del dict["mouthLowerDown_R"]
+del dict["mouthLowerDown_L"]
+
+# Not Related Chew
 #del dict["mouthLowerDown_R"]
 #del dict["mouthLowerDown_L"]
 
-# Not Related < 0.2
-del dict["mouthShrugUpper"]
-del dict["mouthFunnel"]
-del dict["mouthClose"]
+# Not Related Bite
+#del dict["mouthClose"]
+#del dict["mouthFrown_R"]
+#del dict["mouthFrown_L"]
+
+# Related Chew > 0.1
+#del dict["mouthClose"]
+#del dict["mouthFrown_R"]
+#del dict["mouthFrown_L"]
+
+# Not Related Common
+del dict["mouthStretch_R"]
+del dict["mouthStretch_L"]
 del dict["jawForward"]
 del dict["jawRight"]
 del dict["jawLeft"]
-del dict["mouthPucker"]
+del dict["mouthRollUpper"]
+del dict["mouthRollLower"]
+del dict["mouthFunnel"]
+del dict["mouthShrugUpper"]
 del dict["mouthShrugLower"]
 del dict["mouthLeft"]
 del dict["mouthRight"]
 del dict["mouthUpperUp_L"]
 del dict["mouthUpperUp_R"]
-del dict["mouthRollUpper"]
-del dict["mouthRollLower"]
+del dict["mouthPucker"]
 del dict["mouthPress_L"]
 del dict["mouthPress_R"]
-del dict["mouthSmile_L"]
-del dict["mouthSmile_R"]
 del dict["mouthDimple_L"]
 del dict["mouthDimple_R"]
-del dict["mouthFrown_R"]
-del dict["mouthFrown_L"]
-del dict["mouthStretch_R"]
-del dict["mouthStretch_L"]
+del dict["mouthSmile_L"]
+del dict["mouthSmile_R"]
 
 fig, ax = plt.subplots()
 for key in dict:
