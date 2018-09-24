ac3192d52f11c35c4945fa11662bdc9acf698584
Tiago <tiago@MacBook-Pro-de-Tiago.local>
2018-09-24 18:18:06 +0200

    Add Create ML playground

---
+++ MLPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+import CreateML
+import Foundation
+
+let path = Bundle.main.path(forResource: "books", ofType: "csv")
+let bookTable = try MLDataTable(contentsOf: URL(fileURLWithPath: path!))
+
+let builder = try MLClassifier(trainingData: bookTable, targetColumn: "movement")
+
+print(builder.trainingMetrics.classificationError)
+print(builder.model.modelDescription)
+let metadata = MLModelMetadata(author: "TM", shortDescription: "Bite", license: nil, version: "1.0", additional: nil)
+try builder.write(to: URL(fileURLWithPath: "/Users/tiago/Desktop/bites.mlmodel"), metadata: metadata)
